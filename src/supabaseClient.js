0f12a88e348848f8e92e6c0a85ea1c0de86a8e09
israelsanchezdev <63677725+israelsanchezdev@users.noreply.github.com>
2025-04-21 00:48:58 -0500

    🚑 Fix Supabase 406 errors by setting Accept header

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,14 +4,13 @@ const supabaseUrl = 'https://txacnqaxzcjegrthrqzj.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4YWNucWF4emNqZWdydGhycXpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwMjQ3OTAsImV4cCI6MjA2MDYwMDc5MH0.aZ-IR5i_n5KTjIzOzQbxbjaBpSpBOO3UgDa2E9uks20';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  global: {
-    headers: {
-      'Accept': 'application/json',
-    },
-  },
   auth: {
     persistSession: true,
     autoRefreshToken: true,
-    detectSessionInUrl: true,
+  },
+  global: {
+    headers: {
+      Accept: 'application/json',
+    },
   },
 });

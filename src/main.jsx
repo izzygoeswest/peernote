4746edb5f6a1029115bb07e73b6876a46da75fa2
izzygoeswest <izzygoeswest@gmail.com>
2025-04-22 17:56:31 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,18 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { AuthProvider } from './auth';
 
-// global catch for any unhandled promise rejections
-window.addEventListener('unhandledrejection', event => {
+// 🔐 Global catch for any unhandled promise rejections
+window.addEventListener('unhandledrejection', (event) => {
   console.error('Unhandled promise rejection:', event.reason);
 });
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(
-    <React.StrictMode>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </React.StrictMode>
-  );
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);

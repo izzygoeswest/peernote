6276aabd48c67a1793ebf35cd2fd1d73429fc695
izzygoeswest <izzygoeswest@gmail.com>
2025-04-22 17:32:59 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true, // 🔍 enable source maps for debugging
+  },
+  define: {
+    'process.env': {}, // 👈 prevent undefined errors in some packages
+  },
 });

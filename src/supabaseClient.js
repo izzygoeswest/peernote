14abbd0071a68d57bbbbe7f1e406c4b643362887
izzygoeswest <izzygoeswest@gmail.com>
2025-04-23 08:54:06 -0500

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://txacnqaxzcjegrthrqzj.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4YWNucWF4emNqZWdydGhycXpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwMjQ3OTAsImV4cCI6MjA2MDYwMDc5MH0.aZ-IR5i_n5KTjIzOzQbxbjaBpSpBOO3UgDa2E9uks20';
+- const supabaseUrl = 'https://txacnqaxzcjegrthrqzj.supabase.co';
+- const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4YWNucWF4emNqZWdydGhycXpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwMjQ3OTAsImV4cCI6MjA2MDYwMDc5MH0.aZ-IR5i_n5KTjIzOzQbxbjaBpSpBOO3UgDa2E9uks20';
++ const supabaseUrl      = import.meta.env.VITE_SUPABASE_URL;
++ const supabaseAnonKey  = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
